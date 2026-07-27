8bbea08d4a0e4b9188462ff3ae653ff26ca24793
csj9999 <chathuminsasindu@gmail.com>
2026-07-27 15:43:05 +0530

    init

---
+++ src/main/java/com/csj/inventory/EnterpriseInventoryPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.csj.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseInventoryPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnterpriseInventoryPlatformApplication.class, args);
+    }
+
+}
